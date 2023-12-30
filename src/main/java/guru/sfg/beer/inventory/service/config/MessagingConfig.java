d714bee995d62b7d409c21d0e615186ea0da2972
Vladislav Matuizo <dalvmatujzo@gmail.com>
2023-12-30 16:11:50 +0100

    Implemented order allocation logic

--- src/main/java/guru/sfg/beer/inventory/service/config/MessagingConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/MessagingConfig.java
@@ -12,6 +12,7 @@ public class MessagingConfig {
 
     public static final String NEW_INVENTORY_QUEUE_NAME = "new-inventory";
     public static final String ALLOCATE_ORDER_QUEUE_NAME = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE_NAME = "allocate-order-response";
 
 
     @Bean
